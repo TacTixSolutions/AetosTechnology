cc249c94829b374e3316b32da5cb811751495085
Malek Bougadouha <bougadouha.malek@gmail.com>
2025-12-10 12:37:28 +0100

    navbar solution menu changes

---
+++ src/components/icons/hospitalityIcon.tsx
@@ -0,0 +1,77 @@
+function HospitalityIcon() {
+  return (
+    <svg
+      width="26"
+      height="19"
+      viewBox="0 0 26 19"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M10.8445 3.19531L9.49225 4.73414"
+        stroke="white"
+        strokeMiterlimit="10"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M10.8438 3.19694L11.7262 2.19281C11.8672 2.03246 12.0647 1.94531 12.2673 1.94531C12.3445 1.94531 12.4225 1.95796 12.4983 1.98411C12.9136 2.12711 13.106 2.60552 12.9369 3.0025"
+        stroke="white"
+        strokeMiterlimit="10"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M3.30475 5.66147L7.96532 1.42595C8.67194 0.783813 9.59376 0.453417 10.5333 0.505304L17.9973 0.918009C18.2047 0.929471 18.3896 1.02383 18.5228 1.1689C18.6559 1.31397 18.7371 1.50988 18.7371 1.72446C18.7371 2.14246 18.4285 2.49138 18.0255 2.52885L12.9333 3.00182C12.9166 3.04135 12.8961 3.08015 12.872 3.11777L10.453 6.8768C9.64495 8.13241 8.29743 8.9049 6.83882 8.94871L5.48902 8.98927"
+        stroke="white"
+        strokeMiterlimit="10"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M3.64777 11.4179L6.06244 9.85838L2.69914 4.73438L0.500061 6.16019L3.64777 11.4179Z"
+        stroke="white"
+        strokeMiterlimit="10"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M23.8075 15.8671V16.2436H10.7735V15.8671C10.7735 12.3615 13.3656 9.48157 16.6774 9.16232C16.8791 9.14282 17.0837 9.13281 17.2904 9.13281C17.4972 9.13281 17.7016 9.14282 17.9034 9.16232C21.2152 9.48145 23.8075 12.3615 23.8075 15.8671Z"
+        stroke="white"
+        strokeMiterlimit="10"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M25.0927 16.2422H9.48444V17.7778H25.0927V16.2422Z"
+        stroke="white"
+        strokeMiterlimit="10"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M17.8901 7.82031V9.16192C17.6883 9.14242 17.4839 9.13241 17.2771 9.13241C17.0704 9.13241 16.8658 9.14242 16.6641 9.16192V7.82031H17.8901Z"
+        stroke="white"
+        strokeMiterlimit="10"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M20.0608 7.2453C20.0608 7.40359 19.9986 7.54708 19.8983 7.6508C19.7978 7.75464 19.659 7.81885 19.5058 7.81885H15.0706C14.7642 7.81885 14.5157 7.56202 14.5157 7.2453C14.5157 7.087 14.5778 6.94364 14.6782 6.8398C14.7787 6.73608 14.9174 6.67188 15.0706 6.67188H19.5058C19.8123 6.67189 20.0608 6.92858 20.0608 7.2453Z"
+        stroke="white"
+        strokeMiterlimit="10"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M14.1693 11.9531C13.9082 12.2263 13.6027 12.5973 13.3218 13.0775C12.9333 13.7416 12.7372 14.3658 12.6329 14.8199"
+        stroke="white"
+        strokeMiterlimit="10"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  );
+}
+
+export default HospitalityIcon;
