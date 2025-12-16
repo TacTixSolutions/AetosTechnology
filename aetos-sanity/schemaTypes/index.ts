a7628e72cd634721bbd1904066029901dc9d193f
Dali-Hili <74403945+Dali-Hili@users.noreply.github.com>
2025-12-16 12:33:36 +0100

    Prepare repo for Vercel deployment without Sanity Studio

--- aetos-sanity/schemaTypes/index.ts
+++
@@ -1,6 +0,0 @@
-import faq from './faq'
-import testimonial from './testimonial'
-import event from './event'
-import blog from './blog'
-
-export const schemaTypes = [faq, testimonial, event, blog]
--- next.config.ts
+++ next.config.ts
@@ -10,14 +10,6 @@ const nextConfig: NextConfig = {
       },
     ],
   },
-  webpack: (config) => {
-    config.module.rules.push({
-      test: /\.ts$/,
-      include: /aetos-sanity/,
-      use: "ignore-loader",
-    });
-    return config;
-  },
 };
 
 export default nextConfig;
